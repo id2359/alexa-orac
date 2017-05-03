538147bdc0e467bfb10048b815d8cc84cf3c957c
id2359 <id2359@yahoo.com>
2017-05-03 10:07:05 -0400

    still groping around

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // testing codeanywhere
+var APP_ID = "myappd";
+var AlexaSkill = require('./AlexaSkill.js');
+
+var Orac = function() {
+  AlexaSkill.call(this, APP_ID);
+}
+
+Orac.prototype = Object.create(AlexaSkill.prototype);
+Orac.prototype.constructor = Orac;
+
+
+
