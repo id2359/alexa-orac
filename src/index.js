8c2f0a896b353a0f8023cdfff5faf5d8497b1503
id2359 <id2359@yahoo.com>
2017-05-03 10:18:38 -0400

    request started

--- src/index.js
+++ src/index.js
@@ -9,5 +9,10 @@ var Orac = function() {
 Orac.prototype = Object.create(AlexaSkill.prototype);
 Orac.prototype.constructor = Orac;
 
+Orac.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {  
+  console.log("Orac onSessionStarted requestId: " + sessionStartedRequest.requestId  + ", sessionId: " + session.sessionId);  
+  + session.sessionId; 
+};  
+
 
 
